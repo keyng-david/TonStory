32d5edc37977bfc6c389a2cc0a7005149921d2d3
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-10 08:59:31 -0700

    Update index.ts

--- functions/src/scoreboard/index.ts
+++ functions/src/scoreboard/index.ts
@@ -3,16 +3,20 @@ import firestore from "../firestore";
 
 export const getScoreboard = async (req: Request, res: Response) => {
   try {
-    const querySnapshot = await firestore.collection('users').orderBy('points', 'desc').get();
-    const counters = querySnapshot.docs.map((doc: any) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
+    const usersRef = firestore.collection("users");
+    const snapshot = await usersRef
+      .orderBy("points", "desc")
+      .limit(10)
+      .get();
 
-    res.send(counters);
+    const scoreboard = snapshot.docs.map((doc) => doc.data());
+
+    res.status(200).json({
+      message: "Scoreboard retrieved successfully",
+      data: scoreboard,
+    });
   } catch (error) {
-    console.error("Error fetching counters:", error);
-    res.status(500).send('Error fetching scoreboard');
-    
+    console.error("There was an error retrieving the scoreboard", error);
+    res.status(500).send("There was an error retrieving the scoreboard");
   }
-}
+};
