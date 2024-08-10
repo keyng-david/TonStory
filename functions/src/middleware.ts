3e02ef853fa78799128332af699f1d80a8f5df7f
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-10 08:55:21 -0700

    Update middleware.ts

--- functions/src/middleware.ts
+++ functions/src/middleware.ts
@@ -1,40 +1,46 @@
 import { Request, Response } from "express";
-import * as functions from 'firebase-functions';
-import { validate, parse, type InitData } from '@tma.js/init-data-node';
+import * as functions from "firebase-functions";
+import { validate, parse, type InitData } from "@tma.js/init-data-node";
 
 function setInitData(res: Response, initData: InitData): void {
   res.locals.initData = initData;
 }
 
-export const auth = async (request: Request, response: Response, next: any) => {
+export const auth = async (
+  request: Request,
+  response: Response,
+  next: any
+) => {
   try {
     console.log("Request to: ", request.url);
-    console.log('Running auth middleware');
+    console.log("Running auth middleware");
 
-    if (request.url === '/telegram-bot-update') {
+    if (request.url === "/telegram-bot-update") {
       return next();
     }
 
-    const [authType, authData = ''] = (request.header('authorization') || '').split(' ');
+    const [authType, authData = ""] = (
+      request.header("authorization") || ""
+    ).split(" ");
 
     switch (authType) {
-      case 'tma':
+      case "tma":
         try {
           validate(authData, functions.config().tgbot.key, {
             expiresIn: 3600,
           });
           setInitData(response, parse(authData));
-          console.log('Successfully verified token');
+          console.log("Successfully verified token");
           return next();
         } catch (e) {
           return next(e);
         }
       default:
-        return next(new Error('Unauthorized'));
+        return next(new Error("Unauthorized"));
     }
   } catch {
     return response.status(401).json({
-      error: new Error('Invalid request!'),
+      error: new Error("Invalid request!"),
     });
   }
 };
