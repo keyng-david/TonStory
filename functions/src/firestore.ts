37727a00a23b0e6bcd4c362b1d0825b819a53acd
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-10 13:16:20 -0700

    Update firestore.ts

--- functions/src/firestore.ts
+++ functions/src/firestore.ts
@@ -1,4 +1,4 @@
 import * as admin from "firebase-admin";
 const firestore = admin.firestore();
 firestore.settings({ ignoreUndefinedProperties: true });
-export default firestore;
+export default firestore; // Add a newline after this line
