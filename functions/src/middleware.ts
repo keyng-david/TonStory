098aafc2d0a197c3a157aee64c4fca59adf5e44a
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-11 13:04:32 -0700

    Update middleware.ts

--- functions/src/middleware.ts
+++ functions/src/middleware.ts
@@ -2,8 +2,16 @@ import { Request, Response } from "express";
 import * as functions from 'firebase-functions';
 import { validate, parse } from '@tma.js/init-data-node';
 
-function setParsedData(res: Response, parsedData: InitDataParsed): void {
-  res.locals.initData = parsedData;
+function setParsedData(res: Response, parsedData: any): void {
+  const initData = {
+    id: parsedData.userId,
+    firstName: parsedData.user?.firstName || parsedData.first_name,
+    lastName: parsedData.user?.lastName || parsedData.last_name,
+    username: parsedData.user?.username || parsedData.username,
+    languageCode: parsedData.user?.languageCode || parsedData.language_code,
+    // Add any other properties as needed from parsedData
+  };
+  res.locals.initData = initData;
 }
 
 export const auth = async (request: Request, response: Response, next: any) => {
