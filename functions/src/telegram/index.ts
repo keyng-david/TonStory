7d5e5c0a627cb08d5ed4f8c657b983aefeabbb3d
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-11 08:27:43 -0700

    Update index.ts

--- functions/src/telegram/index.ts
+++ functions/src/telegram/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import firestore from "../firestore";
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 import { Telegraf } from 'telegraf';
 import { getOrCreateTelegramUser } from "../users";
 import { formatTonStoryUser } from "../users";
