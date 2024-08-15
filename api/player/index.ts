59d09b9d4df2fa8dd73d99f3100357a8cddd9e02
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-15 06:15:47 -0700

    Update index.ts

--- api/player/index.ts
+++ api/player/index.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import firestore from "../firestore.js";
+import firestore from "../firestore.ts";
 
 export const updatePoints = async (req: Request, res: Response) => {
   console.log("Updating points...");
