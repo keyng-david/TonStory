d91b9806b4ef04c9983c6f6f07abe749bc0ee7b1
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-15 08:30:12 -0700

    Update index.ts

--- api/users/index.ts
+++ api/users/index.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import firestore from "../firestore.ts";
-import { TelegramBotUser, TelegramMiniAppUser, TonStoryUser } from "../types/index.ts";
+import { TelegramBotUser, TelegramMiniAppUser, TonStoryUser } from "../types/index.js";
 
 export const loadUserData = async (req, res) => {
   console.log("Updating points...");
