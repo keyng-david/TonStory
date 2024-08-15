faf72c733e6a77edc755dfcf4464eb104c2aa78d
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-15 03:43:41 -0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,9 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// Removed Firebase-specific imports
+// const { onRequest } = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Vercel-compatible serverless function
+module.exports = (req, res) => {
+  console.log("Hello logs!"); // Using standard console.log for logging
+  res.status(200).send("Hello from Vercel serverless function!");
+};
