5faeb46461bdd9b9940d9e4c48323ff0d0ba0be1
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-15 11:11:27 -0700

    Update firestore.ts

--- api/firestore.ts
+++ api/firestore.ts
@@ -1,14 +1,13 @@
 import admin from 'firebase-admin';
 
-
 // Check if Firebase app is already initialized to avoid re-initialization errors
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
             type: process.env.FIREBASE_TYPE,
-            project_id: console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID.toString(),
+            project_id: process.env.FIREBASE_PROJECT_ID.toString(),
             private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-            private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+            private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
             client_email: process.env.FIREBASE_CLIENT_EMAIL,
             client_id: process.env.FIREBASE_CLIENT_ID,
             auth_uri: process.env.FIREBASE_AUTH_URI,
