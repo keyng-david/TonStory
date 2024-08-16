d9462a25d10bf7a2532bcfb1d44b17fbfb492e17
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-16 00:31:42 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,29 @@
 import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
-import App from "./App";
 import { SDKProvider } from '@tma.js/sdk-react';
-import eruda from 'eruda';
 import { StateProvider } from "./utils/store";
-import { ErrorBoundary } from './components/ErrorBoundary';
-import { lazyInitSDK } from './utils/sdkUtils'; // Lazy load or modularized SDK init
 
-// Initialize eruda only in development mode
-if (process.env.NODE_ENV === 'development') {
+// Conditionally import and initialize Eruda for non-production environments
+if (process.env.NODE_ENV !== 'production') {
+  const eruda = require('eruda');
   eruda.init();
 }
 
+// Lazy load the App component to optimize initial load time
+const App = React.lazy(() => import("./App"));
+
 const container = document.getElementById("root");
 const root = createRoot(container!);
+
 root.render(
-  <ErrorBoundary fallback={<div>Oops! Something went wrong.</div>}>
-    <Suspense fallback={<div>Loading...</div>}>
-      <SDKProvider>
-        <StateProvider>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </StateProvider>
-      </SDKProvider>
-    </Suspense>
-  </ErrorBoundary>
+  <SDKProvider>
+    <StateProvider>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
+      </BrowserRouter>
+    </StateProvider>
+  </SDKProvider>
 );
