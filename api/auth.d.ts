fcbf948cafa05fc5e59c374797856065a9e61201
keyng David <Keyngdavid.c.agu@gmail.com>
2024-08-17 20:27:01 +0000

    Remove unnecessary auth.d.ts file

--- api/auth.d.ts
+++
@@ -1,6 +0,0 @@
-// auth.d.ts
-
-declare module "./auth.js" {
-  export function createJWTToken(payload: { id: string; [key: string]: any }): string;
-  export function verifyJWTToken(token: string): { id: string; [key: string]: any };
-}
