373cfe4384d436d4da66fd18057a85176f00a651
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-17 08:28:20 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 
-
-export default function App() { return ( <div style={{ textAlign: 'center', marginTop: '50px' }}> Ton Story If you see this message, React is working correctly.  ); }
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Ton Story</h1>
+      <p>If you see this message, React is working correctly.</p>
+    </div>
+  );
+}
