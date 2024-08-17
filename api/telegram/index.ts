b1e17d3b87c237877bce7caaa7151ff53f411ee8
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-17 12:50:24 -0700

    Update index.ts

--- api/telegram/index.ts
+++ api/telegram/index.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from "express";
 import { Telegraf } from "telegraf";
-import jwt from "jsonwebtoken";
 import { getOrCreateTelegramUser, formatTonStoryUser } from "../users/index.js";
 import firestore from "../firestore.js";
 
 const botToken = process.env.tgbot;
-const jwtSecret = process.env.JWT_SECRET;
 
-if (!botToken || !jwtSecret) {
-  throw new Error("Bot token or JWT secret is missing");
+if (!botToken) {
+  throw new Error("Bot token is missing");
 }
 
 const bot = new Telegraf(botToken);
@@ -37,18 +35,19 @@ const telegramBotUpdate = async (req: Request, res: Response) => {
         }
       }
 
-      // Generate JWT Token
-      const token = jwt.sign({ id: userData.id }, jwtSecret, { expiresIn: "1h" });
-
       // Check for the "/start" command
       if (message.text === "/start") {
-        const gameUrl = `https://ton-story.vercel.app/?token=${token}`; // Append token to the URL
+        const gameUrl = "https://ton-story.vercel.app/";
 
-        await bot.telegram.sendMessage(userId, `Welcome, ${userData?.firstName || "User"}!\n\nClick the button below to start the game:`, {
-          reply_markup: {
-            inline_keyboard: [[{ text: "Launch", url: gameUrl }]],
-          },
-        });
+        await bot.telegram.sendMessage(
+          userId,
+          `Welcome, ${userData?.firstName || "User"}!\n\nClick the button below to start the game:`,
+          {
+            reply_markup: {
+              inline_keyboard: [[{ text: "Launch", url: gameUrl }]],
+            },
+          }
+        );
       } else {
         await bot.telegram.sendMessage(userId, "Sorry, I didn't understand that.");
       }
