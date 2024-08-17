337e9289b84e43b3093a311983c7bcce15912205
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-16 22:31:22 -0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@
 // const logger = require("firebase-functions/logger");
 
 // Vercel-compatible serverless function
-export default (req, res) => {
+export default (req: Request, res: Response) => {
   console.log("Hello logs!"); // Using standard console.log for logging
   res.status(200).send("Hello from Vercel serverless function!");
 };
