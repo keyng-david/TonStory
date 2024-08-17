c5581a7a6f3595be1b1acb2fddb6110f43d12f08
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-17 11:53:15 -0700

    Update index.ts

--- api/users/index.ts
+++ api/users/index.ts
@@ -1,98 +1,39 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import firestore from "../firestore.js";
 import { TelegramBotUser, TelegramMiniAppUser, TonStoryUser } from "../types/index.js";
 
+const jwtSecret = process.env.JWT_SECRET;
+
+if (!jwtSecret) {
+  throw new Error("JWT secret is missing");
+}
+
 export const loadUserData = async (req: Request, res: Response) => {
-  console.log("Updating points...");
-  const { user } = res.locals.initData;
+  console.log("Loading user data...");
+
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send("Authorization token missing");
+  }
 
   try {
-    const formattedUser = formatTonStoryUser(user);
-    const userData = await getOrCreateTelegramUser(formattedUser);
+    const decoded = jwt.verify(token, jwtSecret) as { id: string };
+    const userRef = firestore.collection("users").doc(decoded.id);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) {
+      return res.status(404).send("User not found");
+    }
+
+    const userData = userDoc.data();
     res.status(200).json({
       message: "User data loaded successfully",
       data: userData,
     });
   } catch (error) {
-    console.error("There was an error loading user", error);
-    res.status(500).send("There was an error loading user");
+    console.error("There was an error loading user data", error);
+    res.status(500).send("There was an error loading user data");
   }
 };
-
-/**
- * Retrieves or creates a Telegram user.
- * @param user - The user data.
- * @param referrer - The referrer ID.
- * @returns The user data.
- */
-export async function getOrCreateTelegramUser(
-  user: TonStoryUser,
-  referrer?: string
-) {
-  const docRef = firestore.collection("users").doc(user.id.toString());
-  return await firestore.runTransaction(async (transaction) => {
-    const doc = await transaction.get(docRef);
-    if (!doc.exists) {
-      // Update referrer points
-      if (referrer) {
-        const referrerDocRef = firestore.collection("users").doc(referrer);
-        const referrerDoc = await transaction.get(referrerDocRef);
-        if (referrerDoc.exists) {
-          transaction.set(
-            referrerDocRef.collection("referrals").doc(user.id.toString()),
-            {
-              userId: user.id,
-              createdAt: new Date(),
-            }
-          );
-          const referrerData = referrerDoc.data();
-          if (referrerData) {
-            transaction.update(referrerDocRef, {
-              referrals: referrerData.referrals + 1,
-            });
-          } else {
-            throw new Error("Referrer data is undefined");
-          }
-        }
-      }
-
-      transaction.set(docRef, user);
-      return user;
-    } else {
-      return doc.data();
-    }
-  });
-}
-
-/**
- * Formats the TonStory user data.
- * @param user - The Telegram user data.
- * @returns The formatted TonStory user data.
- */
-export function formatTonStoryUser(
-  user: TelegramMiniAppUser | TelegramBotUser
-): TonStoryUser {
-  // Determine if the user is a TelegramMiniAppUser
-  const isMiniAppUser = (
-    user: any // Replace 'any' with a more specific type if possible
-  ): user is TelegramMiniAppUser => "allowsWriteToPm" in user;
-
-  return {
-    id: user.id,
-    username: user.username,
-    firstName: "firstName" in user ? user.firstName : user.first_name,
-    lastName: "lastName" in user ? user.lastName : user.last_name,
-    languageCode:
-      "languageCode" in user ? user.languageCode : user.language_code,
-    allowsWriteToPm: isMiniAppUser(user) ? user.allowsWriteToPm : false, // Default to false if not provided
-    level: 1,
-    stamina: 100,
-    points: 0,
-    weapon: 0,
-    referrals: 0,
-    referralURL: `https://t.me/TonStoryBot?start=user_${user.id}`,
-    createdAt: new Date(),
-  };
-}
-
-// Ensure there's a newline at the end of the file
