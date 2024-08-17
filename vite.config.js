cc0e4af3417d0ddc091cfa183c1ef585ab943d5a
keyng David <Keyngdavid.c.agu@gmail.com>
2024-08-17 21:47:24 +0000

    (update)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(), sentryVitePlugin({
+    org: "keyng",
+    project: "javascript-react"
+  })],
+
   define: {
     "process.env": {},
   },
+
+  build: {
+    sourcemap: true
+  }
 });
