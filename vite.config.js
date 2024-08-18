e188f56c8dc4efb22e6f78b83da46b99e6a3e926
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-18 04:37:48 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,11 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(), sentryVitePlugin({
-    org: "keyng",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "keyng",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "keyng",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "keyng",
-    project: "javascript-react-iy"
-  })],
-
+  plugins: [react(), nodePolyfills()],
   define: {
     "process.env": {},
   },
-
-  build: {
-    sourcemap: true
-  }
 });
