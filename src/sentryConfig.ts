467792115ce81f7fa549bd50426ec19717080bd1
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-18 00:30:31 -0700

    Update sentryConfig.ts

--- src/sentryConfig.ts
+++ src/sentryConfig.ts
@@ -1,13 +1,19 @@
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
   dsn: "https://91e7602d4c27ea52a97e6b1d9bb9bbd4@o4507794807717888.ingest.us.sentry.io/4507794931777536",
   integrations: [
-    new BrowserTracing({
-      tracingOrigins: ["localhost", "https://ton-story.vercel.app", /^\//],
-    }) as unknown as Sentry.addIntegration, // Type Assertion
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
   ],
-  tracesSampleRate: 1.0,
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/ton-story\.vercel\.app\/api/],
+  // Set profilesSampleRate to 1.0 to profile every transaction.
   profilesSampleRate: 1.0,
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // Sample rate at 10%. Adjust for production.
+  replaysOnErrorSampleRate: 1.0, // Sample 100% on errors.
 });
