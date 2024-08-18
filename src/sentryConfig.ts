269e736532bef9cf159963c9eb5eaf18688354c3
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-18 00:23:35 -0700

    Update sentryConfig.ts

--- src/sentryConfig.ts
+++ src/sentryConfig.ts
@@ -2,11 +2,11 @@ import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: "your-dsn-here",
+  dsn: "https://91e7602d4c27ea52a97e6b1d9bb9bbd4@o4507794807717888.ingest.us.sentry.io/4507794931777536",
   integrations: [
     new BrowserTracing({
       tracingOrigins: ["localhost", "https://ton-story.vercel.app", /^\//],
-    }) as unknown as Sentry.Integration, // Type Assertion
+    }) as unknown as Sentry.addIntegration, // Type Assertion
   ],
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
