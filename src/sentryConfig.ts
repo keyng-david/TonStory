add37fb58c033e9991156cf158237d9562a084d3
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-18 00:11:58 -0700

    Update sentryConfig.ts

--- src/sentryConfig.ts
+++ src/sentryConfig.ts
@@ -2,16 +2,12 @@ import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
-  dsn: "https://91e7602d4c27ea52a97e6b1d9bb9bbd4@o4507794807717888.ingest.us.sentry.io/4507794931777536",
+  dsn: "your-dsn-here",
   integrations: [
     new BrowserTracing({
-      // Replacing `tracingOrigins` with `tracePropagationTargets`
-      tracePropagationTargets: ["localhost", "https://ton-story.vercel.app/", /^\//],
-      // Additional configuration if needed
-      // instrumentPageLoad: true,
-      // instrumentNavigation: true,
-    }),
+      tracingOrigins: ["localhost", "https://ton-story.vercel.app", /^\//],
+    }) as unknown as Sentry.Integration, // Type Assertion
   ],
   tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0, // Include if you need profiling
+  profilesSampleRate: 1.0,
 });
