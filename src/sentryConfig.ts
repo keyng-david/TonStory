a1c8861cac506e85d5d263e3e1b8a30730379055
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-17 23:15:00 -0700

    Update sentryConfig.ts

--- src/sentryConfig.ts
+++ src/sentryConfig.ts
@@ -1,15 +1,14 @@
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/tracing";
+import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
   dsn: "https://91e7602d4c27ea52a97e6b1d9bb9bbd4@o4507794807717888.ingest.us.sentry.io/4507794931777536",
   integrations: [
-    new BrowserTracing({
+    new Integrations.BrowserTracing({
       tracingOrigins: ["localhost", "yourdomain.com", /^\//],
       // Adjust this to match your needs
     }),
   ],
   tracesSampleRate: 1.0,
-  // Include the profiling options if required:
   profilesSampleRate: 1.0,
 });
