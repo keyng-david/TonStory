ba00cd7181803afafb87b1063f87470bbfb6aac6
keyng-david <61462535+keyng-david@users.noreply.github.com>
2024-08-18 23:45:44 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import { Route, Routes } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 // import Game from "./pages.tsx/Game"; 
-import Shop from "./pages.tsx/Shop"; // Uncomment this line to test Shop route
-// import Share from "./pages.tsx/Share"; // Uncomment this line to test Share route
+// import Shop from "./pages.tsx/Shop"; // Uncomment this line to test Shop route
+import Share from "./pages.tsx/Share"; // Uncomment this line to test Share route
 // import Scoreboard from "./pages.tsx/Scoreboard"; // Uncomment this line to test Scoreboard route
 
 function ErrorBoundary({ children }: { children: React.ReactNode }) {
@@ -28,7 +28,7 @@ export default function App() {
         <h2>{debugMessage}</h2>
         <Routes>
           {/* Test Game Route */}
-          <Route path="/" element={<Shop />} />
+          <Route path="/" element={<Share />} />
 
           {/* Test Shop Route */}
           {/* <Route path="/" element={<Shop />} /> */}
